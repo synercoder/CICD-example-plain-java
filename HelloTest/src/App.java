4061c9d17e3452262abc5c2035214a7b337e0592
Gerard Gunnewijk <gerard.gunnewijk@live.nl>
2025-01-29 15:38:43 +0000

    Added calculator usage to App.java

--- HelloTest/src/App.java
+++ HelloTest/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        var calculator = new Calculator();
         System.out.println("Hello, World!");
+        System.out.println("1 + 1 = " + calculator.Add(1, 1));
     }
 }
